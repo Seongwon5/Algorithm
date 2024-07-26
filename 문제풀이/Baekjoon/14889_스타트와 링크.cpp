bd2a0dae45a3cff88cbd8b3a7271c8ce9dca8251
Seongwon5 <139881766+Seongwon5@users.noreply.github.com>
2024-07-26 14:20:33 +0000

    commit

--- 문제풀이/Baekjoon/14889_스타트와 링크.cpp
+++ 문제풀이/Baekjoon/14889_스타트와 링크.cpp
@@ -36,8 +36,6 @@ int getDiff(int**ability,int n,int*currentTeam){
         }
     }
     //전체 n개의 숫자 중 currentteam에 없는 숫자들을 enemyTeam으로 결정한다.
-    //모든 ability의 합은 ab 와 ba의 경우가 다르므로 단순히 n size의 for문을 2개 돌림으로써 모든 ability의
-    //총합을 계산할 수 있다. getmain처럼 모든 순서를 구하는 것이 아니라 2명의 순서쌍만 구하면 된다.
     //cout<<"능력치는"<<mTeamAbility<<"대 "<<eTeamAbility<<"입니다"<<endl;
     int diff=mTeamAbility-eTeamAbility;
     if(diff>=0) return diff;
