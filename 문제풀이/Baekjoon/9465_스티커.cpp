d0ce0744ad0cc82058f2a1df75895bb01ceb189b
Seongwon5 <139881766+Seongwon5@users.noreply.github.com>
2024-08-24 10:12:41 +0000

    commit

--- 문제풀이/Baekjoon/9465_스티커.cpp
+++ 문제풀이/Baekjoon/9465_스티커.cpp
@@ -1,31 +1,32 @@
 #include<iostream>
 using namespace std;
 
-int cache[100000][3]//0번: 위쪽 칸을 고르는 경우, 1번: 아랫쪽 칸을 고르는 경우, 2번: 아무 칸도 고르지 않는 경우
-int solve(int*stickers[][2],int n,int i, int choice){//i번 이상의 스티커로 만드는 최댓값
+int cache[100000][3];//0번: 이전에 위쪽 칸을 고른 경우, 1번: 이전에 아랫쪽 칸을 고른 경우, 2번: 이전에 아무 칸도 고르지 않은 경우
+int solve(int stickers[][2],int n,int i, int choice){//i번 이상의 스티커로 만드는 최댓값
     int &ret=cache[i][choice];
     if(ret!=-1) return ret;
-    if(i==n-1){
+    if(i==n-1){//기저 사례: 마지막 칸에 도달한 경우
         if(choice==0){
-            return ret=
+            return ret=stickers[i][1];//아래 칸을 선택한다.
         }
         else if(choice==1){
-            return ret=
+            return ret=stickers[i][0];//위쪽 칸을 선택한다.
         }
-        else{//choice==3
-            return ret=0;
+        else{//이전에 아무 칸도 선택하지 않았던 경우
+            return ret=max(stickers[i][0],stickers[i][1]);//두 칸 중에 더 큰 것을 선택한다.
         }
     }
-    if(choice==0){
-        ret=max(ret,stickers[n][0]+max(solve(stickers, n+1,1), solve(stickers, n+1,2)));
+    if(choice==0){//이전에 위쪽 칸을 골랐던 경우. 아래 칸을 고르든지, 아무것도 안 고르든지 둘 중 하나.
+        ret=max(stickers[i][1]+solve(stickers, n, i+1, 1),solve(stickers, n, i+1, 2));
     }
     else if(choice==1){
-        ret=stickers[n][1]+max(solve(stickers, n+1,0), solve(stickers, n+1,2));
+        ret=max(stickers[i][0]+solve(stickers, n, i+1, 0),solve(stickers, n, i+1, 2));
     }
-    else{//choice==3
-        ret=max(solve(stickers, n+1,0), solve(stickers, n+1,1));
-        ret=max(ret,solve(stickers, n+1,2));
+    else{//choice==2//아무것도 고르지 않은 경우: 어떤 선택을 하든 상관없다.
+        ret=max(stickers[i][0]+solve(stickers, n, i+1, 0),stickers[i][1]+solve(stickers, n, i+1, 1));
+        ret=max(ret,solve(stickers, n, i+1, 2));
     }
+    return ret;//구한 최댓값을 리턴한다.
 }
 int main(){
     int T;//테스트 케이스의 개수
@@ -46,5 +47,8 @@ int main(){
                 cin>>stickers[j][i];
             }
         }
+
+        cout<<solve(stickers, n , 0, 2)<<endl;//0번째 칸은 이전 칸이 없지만, 이전에 아무것도 고르지 않은 경우와 동일하다.
     }
+
 }
