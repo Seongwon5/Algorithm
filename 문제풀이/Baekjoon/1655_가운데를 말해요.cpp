628c88aec2be848ca2cecae79cf2b2bf7308cb90
Seongwon5 <139881766+Seongwon5@users.noreply.github.com>
2024-09-14 07:13:48 +0000

    commit

---
+++ 문제풀이/Baekjoon/1655_가운데를 말해요.cpp
@@ -0,0 +1,41 @@
+#include<iostream>
+#include<queue>
+using namespace std;
+
+int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    priority_queue<int> minQueue;
+    priority_queue<int> maxQueue;
+
+    int N;
+    cin>>N;
+    for(int i=0;i<N;i++){
+        int x;
+        cin>>x;
+
+        if(i==0){
+            maxQueue.push(x);
+            cout<<x<<'\n';
+        }
+        else{
+            if(maxQueue.top()<x){
+                minQueue.push(-x);
+                if(maxQueue.size()<minQueue.size()){
+                    int mintop=-minQueue.top();
+                    minQueue.pop();
+                    maxQueue.push(mintop);
+                }
+            }
+            else{
+                maxQueue.push(x);
+                if(maxQueue.size()>minQueue.size()+1){
+                    int maxtop=maxQueue.top();
+                    maxQueue.pop();
+                    minQueue.push(-maxtop);
+                }
+            }
+            cout<<maxQueue.top()<<'\n';
+        }
+    }
+}
