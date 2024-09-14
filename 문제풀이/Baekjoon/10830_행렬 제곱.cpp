acd7d44950e2ff37d26f77e4dbf5932d97672a52
Seongwon5 <139881766+Seongwon5@users.noreply.github.com>
2024-09-14 23:24:06 +0000

    great

--- 문제풀이/Baekjoon/10830_행렬 제곱.cpp
+++ 문제풀이/Baekjoon/10830_행렬 제곱.cpp
@@ -15,10 +15,32 @@ vector<vector<long long>> mul(vector<vector<long long>>&P,vector<vector<long lon
     for(int i=0;i<N;i++){
         for(int j=0;j<N;j++){
             //현재 i,j 원소의 값을 결정하고 있음. 이 경우 P의 i행과 Q의 j열 원소들을 1대1로 대응시킴.
+            //행렬의 각 원소는 그것을 1000으로 나눈 나머지이다.
             for(int k=0;k<N;k++){
-                ret[i][j]+=P[i][k]*Q[k][j];
+                ret[i][j]+=(P[i][k]*Q[k][j])%1000;
+            }
+            ret[i][j]%=1000;
+        }
+    }
+    return ret;
+}
+vector<vector<long long>> power(vector<vector<long long>>&A, long long B){
+    if(B==1){//기저 사례: A의 1승
+        for(int i=0;i<N;i++){
+            for(int j=0;j<N;j++){
+                A[i][j]%=1000;
             }
         }
+        return A;
+    }
+    if(B%2==0){//짝수승: 정확히 반 크기로 가른다.
+        vector<vector<long long>> multiplied= power(A,B/2);
+        return mul(multiplied,multiplied);
+    }
+    else{//홀수승: 1개를 떼어내고 반 크기로 가른다.
+        vector<vector<long long>> multiplied= power(A,(B-1)/2);
+        vector<vector<long long>> mid= mul(multiplied,multiplied);
+        return mul(mid,A);
     }
 }
 int main(){
@@ -29,6 +51,13 @@ int main(){
         for(int j=0;j<N;j++){
             cin>>A[i][j];
         }
+    }
+    vector<vector<long long>> ret= power(A,B);
+    for(int i=0;i<N;i++){
+        for(int j=0;j<N;j++){
+            cout<<ret[i][j]<<' ';
+        }
+        cout<<endl;
     }
 
 
